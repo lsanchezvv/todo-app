5ba814dfd69f8cedb1e23dbaeeb9d23a8b47fac8
Luis Sanchez <lsanchez@vivial.net>
2020-07-14 18:57:44 -0400

    changes fa to cdn for now

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
 import './Todo.css'
 
 
@@ -12,8 +10,12 @@ function Todo ({ todo, index, completeTodo, removeTodo }) {
       className="todo">
       {todo.text}
       <div className="todo-action">
-        <FontAwesomeIcon title="Mark as complete" size="2x" onClick={() => completeTodo(index)} icon={faCheck}></FontAwesomeIcon>
-        <FontAwesomeIcon title="Remove todo" size="2x" onClick={() => removeTodo(index)} icon={faTimes}></FontAwesomeIcon>
+        <span onClick={() => completeTodo(index)} className="todo-icons">
+          <i title="Mark as complete" size="2x" class="fas fa-check"></i>
+        </span>
+        <span onClick={() => removeTodo(index)} className="todo-icons">
+          <i title="Mark as complete" size="2x" class="fas fa-times"></i>
+        </span>
       </div>
     </div>
   )
